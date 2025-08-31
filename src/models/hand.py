49f5da6f62167392f6fd7df66a0a70a95308a4c7
Daniel <danieldlopesf@gmail.com>
2025-08-31 16:59:55 -0300

    commit inicial

---
+++ src/models/hand.py
@@ -0,0 +1,3 @@
+class Hand:
+    def __init__(self, pieces):
+        self.pieces = pieces
---
+++ src/models/stone.py
@@ -0,0 +1,4 @@
+class Stone:
+    def __init__(self, number1, number2):
+        self.number1 = number1
+        self.number2 = number2
