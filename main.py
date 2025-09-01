54b6d56750d4abee23fe9d765b391d0d301d5dbe
Daniel <danieldlopesf@gmail.com>
2025-09-01 01:19:42 -0300

    feat: init do jogo de domino

---
+++ main.py
@@ -0,0 +1,3 @@
+from src.models import Domino
+
+game = Domino.init_game()
--- src/models/table.py
+++ src/models/table.py
@@ -1,12 +1,12 @@
 import networkx as nx # Lib de Grafos
 
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel, ConfigDict, Field
 
 from .dorme import Dorme
 from .stone import Stone
 
 class Table(BaseModel):
     dorme: Dorme
-    played_stones: nx.MultiGraph # Representação do jogo no formato de um grafo
+    played_stones: nx.MultiGraph = nx.MultiGraph()
 
     model_config = ConfigDict(arbitrary_types_allowed=True)
